1040c232c027b51dec0df0e4816aeed7da69cff4
Noella <nhoro@andrew.cmu.edu>
2025-05-30 22:47:01 +0530

    temp change for deployment

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from albumy import create_app
 
 
 app = create_app()
+
+app = create_app()
+
+with app.app_context():
+    from albumy.extensions import db
+    db.create_all()  # remove this after first deploy
+
